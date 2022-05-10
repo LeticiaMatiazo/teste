dd3e82b378893705f653bf47ac8eba5fdbdf7b6a
Letícia-Matiazo <leticia.mattos1990@gmail.com>
2022-05-10 12:23:07 -0300

    teste

---
+++ git_codigos.js
@@ -0,0 +1,30 @@
+// git init  => Inicializa Repositório Local;
+
+// git branch feature_2  => Cria branch com o nome feature_2;
+// git checkout feature_2  => Muda para a branch feature_2;
+
+// git checkout -b feature_2  => Cria e Muda para branch feature_2, substitui os dois últimos comandos;
+
+// git status  => Status do repositório, caso esteja vermelho significa mudanças que ainda estão no pc,
+//                 caso esteja verde significa que estão no staging index, ou seja, podem dar commit;
+
+// git add nome_do_arquivo  => Adiciona arquivo no staging index, para poder dar commitar, (o arquivo em vermelho);
+
+// git add .  => Adiciona todos os aquivos de mudanças no staging index, (os arquivos em vermelho);
+
+// git commit -m "MENSAGEM DE COMMIT"  => Cria versão das alterações;
+
+// git merge feature_1  => Mescla a feature_1 na branch atual;
+
+// git branch  => Mostra todas as branches;
+
+// git remote add origin https://github.com/meu_usuario/meu_repo.git  => Define qual sera o repositório remoto;
+
+// git branch -M main  => Define a main como branch principal;
+
+// git push -u origin main  => Envia a branch main para o repositório remoto;
+
+// git config user.name "MEU_NOME" => Configura o nome de usuário;
+// git config user.email "MEUEMAIL@EMAIL.COM" => Configura o email do usuário;
+
+console.log("Letícia Matiazo")
